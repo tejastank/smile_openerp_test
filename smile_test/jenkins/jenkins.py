6cc46ff1dc8eb4c7bf77774693436e900e21cce8
Corentin PB <corentin.pb@gmail.com>
2012-09-07 16:41:43 +0200

    [IMP] Drop database after installation/update/test failing

--- smile_test/jenkins/jenkins.py
+++ smile_test/jenkins/jenkins.py
@@ -316,23 +316,25 @@ if __name__ == '__main__':
     server.start()
     # Create test database
     db = server.create_timed_db(demo=conf.get('demo', True), lang=conf.get('lang', 'fr_FR'), user_password='admin')
-    db.install_modules(['smile_test'])
-    server.sock_common.coverage_start(True, source_dir.python_files)
-    # Install config's modules
-    module_list = conf['module_list'].split(',')
-    db.install_modules(module_list)
-    time.sleep(5)
-    ignore_tests = conf.get('ignore_tests', False)
-    if ignore_tests:
-        try:
-            ignore_tests = eval(ignore_tests)
-        except (NameError, SyntaxError):
-            ignore_tests = False
-    db.sock_exec('smile.test', 'test_to_xunitfile', 'all', conf['xunit_file'], ignore_tests)
-    time.sleep(5)
-    server.sock_common.coverage_stop_and_save(conf.get('coverage_file'), source_dir.python_files)
-    time.sleep(1)
-    # Drop db
-    db.drop()
+    try:
+        db.install_modules(['smile_test'])
+        server.sock_common.coverage_start(True, source_dir.python_files)
+        # Install config's modules
+        module_list = conf['module_list'].split(',')
+        db.install_modules(module_list)
+        time.sleep(5)
+        ignore_tests = conf.get('ignore_tests', False)
+        if ignore_tests:
+            try:
+                ignore_tests = eval(ignore_tests)
+            except (NameError, SyntaxError):
+                ignore_tests = False
+        db.sock_exec('smile.test', 'test_to_xunitfile', 'all', conf['xunit_file'], ignore_tests)
+        time.sleep(5)
+        server.sock_common.coverage_stop_and_save(conf.get('coverage_file'), source_dir.python_files)
+        time.sleep(1)
+    finally:
+        # Drop db
+        db.drop()
     # Kill OpenERP
     server.kill()
