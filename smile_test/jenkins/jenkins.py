848bf82e5febe316e01f0a5cdb4990580f756a3e
xavfernandez <xav.fernandez@gmail.com>
2012-09-25 13:42:03 +0200

    [IMP] set logging level to TEST

--- smile_test/jenkins/jenkins.py
+++ smile_test/jenkins/jenkins.py
@@ -361,7 +361,7 @@ if __name__ == '__main__':
     # Start OpenERP
     # disable_test, since we will run them through smile_test
     server = ServerProxy(conf['manual_launch_command'], conffile='openerp.conf',
-                         log_level='test', log_handler=':WARNING', test_disable=True, version=conf.get('openerp_version', '6.0'))
+                         log_level='test', log_handler=':TEST', test_disable=True, version=conf.get('openerp_version', '6.0'))
     server.start()
     # Create test database
     db = server.create_timed_db(demo=conf.get('demo', True), lang=conf.get('lang', 'fr_FR'), user_password='admin')
